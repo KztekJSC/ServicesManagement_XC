635b13958bf57e511567d39c7b6b2fb1ef0c80c7
Hà Lâm <hangoclam82@gmail.com>
2021-11-22 14:28:57 +0700

    làm api

--- Kztek_Model/Models/tbl_Event.cs
+++ Kztek_Model/Models/tbl_Event.cs
@@ -38,7 +38,7 @@ namespace Kztek_Model.Models
 
         public decimal SubPrice { get; set; } //Phụ thu
 
-        public string SubGroup { get; set; } //Tổ bốc xếp
+        public string GroupId { get; set; } //Tổ bốc xếp
 
         public DateTime CreatedDate { get; set; } //Ngày tạo
 
@@ -50,6 +50,26 @@ namespace Kztek_Model.Models
         public string ImageCN { get; set; } //ảnh xe Trung Quốc
         public string ServiceCode { get; set; } //Số trang (mã dịch vụ)
         public string ParkingPosition  { get; set; }//Vị trí đỗ
+        public bool IsDeleted { get; set; } //true là xóa
     }
 
+    public class tbl_Event_POST
+    {
+        public string Service { get; set; } //Dịch vụ
+        public string Code { get; set; } //Mã đăng ký
+        public int EventType { get; set; } //Trạng thái
+        public string PlateVN { get; set; } //Biển số xe Việt Nam
+        public string ImageVN { get; set; } //ảnh xe Việt Nam
+        public string PlateCN { get; set; } //Biển số xe Trung Quốc
+        public string ImageCN { get; set; } //ảnh xe Trung Quốc
+        public string ProductType { get; set; } //Loại hàng
+        public int Weight { get; set; } //Khối lượng hàng    
+        public string VehicleType { get; set; } //Loại xe
+        public string ProductGroup { get; set; } //Nhóm hàng
+        public decimal Price { get; set; } //Giá dịch vụ
+        public decimal SubPrice { get; set; } //Phụ thu
+        public string Description { get; set; }
+        public string ServiceCode { get; set; } //Số trang (mã dịch vụ)
+        public string ParkingPosition { get; set; }//Vị trí đỗ
+    }
 }
---
+++ Kztek_Service/Api/Database/SQLSERVER/tbl_EventService.cs
@@ -0,0 +1,109 @@
+﻿using Kztek_Core.Models;
+using Kztek_Data.Repository;
+using Kztek_Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kztek_Service.Api.Database.SQLSERVER
+{
+    public class tbl_EventService
+    {
+        private Itbl_EventRepository _tbl_EventRepository;
+
+        public tbl_EventService(Itbl_EventRepository _tbl_EventRepository)
+        {
+            this._tbl_EventRepository = _tbl_EventRepository;
+        }
+        public async Task<tbl_Event> GetById(string id)
+        {
+            return await _tbl_EventRepository.GetOneById(id);
+        }
+
+        public async Task<MessageReport> Create(tbl_Event_POST model)
+        {
+            var obj = new tbl_Event()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Code = model.Code,
+                ImageCN = model.ImageCN,
+                ImageVN = model.ImageVN,
+                PlateCN = model.PlateCN,
+                PlateVN = model.PlateVN,
+                Description = model.Description,
+                GroupId = "",
+                Price = model.Price,
+                SubPrice = model.SubPrice,
+                ServiceCode = model.ServiceCode,
+                Service = model.Service,
+                ParkingPosition = "",
+                VehicleType = model.VehicleType,
+                Weight = model.Weight,
+                ProductType = model.ProductType,
+                ProductGroup = model.ProductGroup,
+                EventType = model.EventType,
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now,
+                StartDate = DateTime.MinValue,
+                EndDate = DateTime.MinValue
+            };
+
+            var result = await _tbl_EventRepository.Add(obj);
+
+            return result;
+        }
+
+        public async Task<MessageReport> Update(tbl_Event_POST model)
+        {
+            var result = new MessageReport(false, "Có lỗi xảy ra");
+
+            var obj = await GetById(model.Code);
+            if (obj == null)
+            {
+                result = new MessageReport(false, "Bản ghi không tồn tại");
+                return await Task.FromResult(result);
+            }
+
+            obj.Code = model.Code;
+            obj.ImageCN = model.ImageCN;
+            obj.ImageVN = model.ImageVN;
+            obj.PlateCN = model.PlateCN;
+            obj.PlateVN = model.PlateVN;
+            obj.Description = model.Description;
+            obj.GroupId = "";
+            obj.Price = model.Price;
+            obj.SubPrice = model.SubPrice;
+            obj.ServiceCode = model.ServiceCode;
+            obj.Service = model.Service;
+            obj.ParkingPosition = "";
+            obj.VehicleType = model.VehicleType;
+            obj.Weight = model.Weight;
+            obj.ProductType = model.ProductType;
+            obj.ProductGroup = model.ProductGroup;
+            obj.EventType = model.EventType;
+            obj.ModifiedDate = DateTime.Now;
+
+            result = await _tbl_EventRepository.Update(obj);
+
+            return result;
+        }
+
+        public async Task<MessageReport> Delete(string id)
+        {
+            var result = new MessageReport(false, "Có lỗi xảy ra");
+
+            var obj = await GetById(id);
+            if (obj == null)
+            {
+                result = new MessageReport(false, "Bản ghi không tồn tại");
+                return result;
+            }
+
+            result = await _tbl_EventRepository.Remove(obj);
+
+            return result;
+        }
+    }
+}
